69aaf27df45317be5b82875da77db8de19a13172
Benedicte Helen Myrvoll <benedicte@Benedictes-MacBook-Pro.local>
2020-06-29 13:45:35 +0200

    Updates

---
+++ precache-manifest.fde189e36cb9181ce6e2469803254a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816a0a816ea71f33ef6ecdab85c825ae",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "c7af8af650f219c1fb0a",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e0881c970955aab779f",
+    "url": "/infoskjerm/static/js/2.2b71e3f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.2b71e3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7af8af650f219c1fb0a",
+    "url": "/infoskjerm/static/js/main.ee47c097.chunk.js"
+  },
+  {
+    "revision": "3e63cd5e5c68c684204f",
+    "url": "/infoskjerm/static/js/runtime-main.e679cb49.js"
+  }
+]);
